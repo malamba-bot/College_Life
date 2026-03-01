2d62752ac476d193ede99c3e7c1396db71cb5fd3
Max Kinet <maxkinet@gmail.com>
2026-02-28 22:21:09 -0800

    add windows 95 desktop

--- src/scenes/Bootup.js
+++ src/scenes/Bootup.js
@@ -5,10 +5,12 @@ class Bootup extends Phaser.Scene {
 
     preload() {
         this.load.image('canvas_icon', './assets/imgs/canvas_icon.png');
+        this.load.image('w95_desktop', './assets/imgs/w95_desktop.png');
 
     }
 
     create() {
+        this.add.image(width / 2, height / 2, 'w95_desktop').setDisplaySize(width, height);
         this.add.image(width / 2, height / 2, 'canvas_icon').setScale(0.05);
     }
 }
