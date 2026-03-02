90368d02a09c11ec671f720e76f103bf4652bd2d
Max Kinet <maxkinet@gmail.com>
2026-03-02 14:23:10 -0800

    add rex textbox plugin

--- src/scenes/Canvas.js
+++ src/scenes/Canvas.js
@@ -1,9 +1,19 @@
+/* CHANGES TBM: change plugin load method
+    */
 class Canvas extends Phaser.Scene {
     constructor() {
         super('Canvas');
     }
+
+    preload() {
+        // Yoinked from https:/rexrainbow.github.io/phaser3-rex-notes/docs/site/ui-textbox/
+        // Load the textbox plugin from the internet (relies on the github being active) 
+        this.load.scenePlugin('rexuiplugin', 'https://raw.githubusercontent.com/rexrainbow/phaser3-rex-notes/master/dist/rexuiplugin.min.js', 'rexUI', 'rexUI');
+    }
+
     create() {
         this.add.text(width/2, height/2, 'This be the Canvas scene');
+        this.textBox = this.rexUI.add.textBox();
 
     }
 }
