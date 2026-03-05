197837e087bf479f6aa61a944b5ef9053c883af3
Max Kinet <mkinet@ucsc.edu>
2026-03-04 21:45:42 -0800

    add config object literal to interactiveTextBox object

--- src/prefabs/InteractiveTextBox.js
+++ src/prefabs/InteractiveTextBox.js
@@ -1,14 +1,21 @@
 class InteractiveTextBox extends Phaser.GameObjects.Container {
-    constructor(scene, x, y) {
+    constructor(scene, x, y, config = {
+        height: 100,
+        width : 100,
+        color: 0xffffff}) {
         super(scene, x, y);
-        this.box = scene.add.rectangle(x, y, 200, 200, 0x333333);
-        console.log("I am inside the class");
+
+        // Save config
+        this.config = config;
+
+        // Add background
+        this.scene.add.rectangle(x, y, this.config.width, this.config.height, this.config.color);
     }
 
 }
 
 // Register this object with Phaser's object factory
-Phaser.GameObjects.GameObjectFactory.register('interactiveTextBox', function (x, y) {
-    return this.displayList.add(new InteractiveTextBox(this.scene, x, y));
+Phaser.GameObjects.GameObjectFactory.register('interactiveTextBox', function (x, y, config) {
+    return this.displayList.add(new InteractiveTextBox(this.scene, x, y, config));
 });
 
--- src/scenes/Canvas.js
+++ src/scenes/Canvas.js
@@ -4,12 +4,16 @@ class Canvas extends Phaser.Scene {
     }
 
     preload() {
+
     }
 
     create() {
-        //this.add.text(width/2, height/2, 'This be the Canvas scene');
-        this.add.interactiveTextBox(100,  200);
-        console.log(this.sys.displayList.list);
+        this.text_box_config = {
+            width: 400,
+            height: 200,
+            color: 0xff0000
+        };
 
+        this.add.interactiveTextBox(width / 2 , height / 2, this.text_box_config);
     }
 }
