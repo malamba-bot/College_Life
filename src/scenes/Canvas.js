ed8728f49df10648e68380e45d259af37d9efda7
Max Kinet <mkinet@ucsc.edu>
2026-03-04 22:20:27 -0800

    fix: interactiveTextBox merges default and user-supplied config

--- src/scenes/Canvas.js
+++ src/scenes/Canvas.js
@@ -9,9 +9,12 @@ class Canvas extends Phaser.Scene {
 
     create() {
         this.text_box_config = {
-            width: 400,
-            height: 200,
-            color: 0xff0000
+            width: TEXTBOX_WIDTH,
+            height: TEXTBOX_HEIGHT,
+            color: 0xffffff,
+            alpha: 0,
+            stroke_thickness: 3,
+            stroke_color: COLORS.BLUE
         };
 
         this.add.interactiveTextBox(width / 2 , height / 2, this.text_box_config);
