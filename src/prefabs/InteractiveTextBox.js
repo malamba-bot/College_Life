ac2719c4ea7aebae920cffd8d6f4187b082f68b4
Max Kinet <mkinet@ucsc.edu>
2026-03-04 22:57:12 -0800

    add input listener for keypresses and text to interactiveTextBox

--- src/prefabs/InteractiveTextBox.js
+++ src/prefabs/InteractiveTextBox.js
@@ -4,7 +4,8 @@ const default_config = {
     color: 0xffffff,
     stroke_thickness: 5,
     stroke_color: 0xffffff,
-    stroke_alpha: 1
+    stroke_alpha: 1,
+    // TODO add text options here?
 }
 
 class InteractiveTextBox extends Phaser.GameObjects.Container {
@@ -14,13 +15,34 @@ class InteractiveTextBox extends Phaser.GameObjects.Container {
         // Merge default and user-supplied config
         this.config = {...default_config, ...config };
 
-        // Add background
+        this.add_background(x, y);
+
+        
+        this.scene.input.keyboard.on('keydown', (key_pressed) => {console.log(`Pressed ${key_pressed.key}`)});
+
+        this.text = scene.add.text(x - this.config.width / 2, y - this.config.height / 2, "Hello FRRR", {
+            fontSize: '24px',
+            padding: {
+                left: 20,
+                right: 20,
+                top: 20,
+                bottom: 20,
+            },
+            fixedWidth: this.config.width
+        }).setOrigin(0);
+    }
+
+    add_background(x, y) {
         this.background = this.scene.add.rectangle(x, y, this.config.width, this.config.height, this.config.color, this.config.alpha);
-        console.log(this.config.stroke_thickness);
         this.background.setStrokeStyle(this.config.stroke_thickness, this.config.stroke_color, this.config.stroke_alpha);
     }
 
+    preUpdate(time, delta) {
+        super.preUpdate(time, delta);
+    }
+
 }
+
 
 // Register this object with Phaser's object factory
 Phaser.GameObjects.GameObjectFactory.register('interactiveTextBox', function (x, y, config) {
