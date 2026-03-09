92e59791aa0cbaa34fff997fbc8708b13b05815e
Max Kinet <mkinet@ucsc.edu>
2026-03-09 12:55:32 -0700

    add submit button and have it compare textbox input and assignment strings

--- src/main.js
+++ src/main.js
@@ -20,10 +20,25 @@ let game = new Phaser.Game(config);
 
 let { height, width } = game.config;
 
-const TEXTBOX_WIDTH = width * 0.5;
+// Textbox configuration constants
+const TEXTBOX_WIDTH = width * 0.55;
 const TEXTBOX_HEIGHT = height * 0.3;
 const TEXTBOX_PADDING = 20;
+
+// Submit button configuration constants
+const SUBMIT_WIDTH = 150;
+const SUBMIT_HEIGHT = 50;
+const SUBMIT_X = width * 0.5 + TEXTBOX_WIDTH * 0.5 - SUBMIT_WIDTH * 0.5;
+const SUBMIT_Y = height * 0.95;
 
 const COLORS = {
     BLUE: 0x1f7c9e
 }
+
+// Correct assignment strings
+const assignment_text_1 =
+    `#include <stdio.h>
+
+    int main(void) {
+        printf("Hello, world!\n");
+    }`
--- src/scenes/Canvas.js
+++ src/scenes/Canvas.js
@@ -4,11 +4,32 @@ class Canvas extends Phaser.Scene {
     }
 
     create() {
+        // Set up assignments
+        this.assignment = 0;
+        this.assignment_text = [assignment_text_1];
+
         // Add Canvas background
         this.add.image(0, 0, 'canvas_assignment').setDisplaySize(width, height).setOrigin(0);
 
         // Add assignment
         this.add.image(width / 2, height * 0.35, 'assignment_1').setDisplaySize(width * 0.55, height / 2);
+
+        // Create submit button
+        this.submit_button = this.add.rectangle(SUBMIT_X, SUBMIT_Y, SUBMIT_WIDTH, SUBMIT_HEIGHT, 0x327fba);
+        this.submit_text = this.add.text(SUBMIT_X, SUBMIT_Y, 'Submit', {
+            fontFamily: 'Arial',
+            fontSize: '32px',
+            align: 'center'
+        }).setOrigin(0.5);
+        this.submit_button.setInteractive({cursor: 'pointer'});
+        this.submit_button.on('pointerdown', () => {
+            console.log(this.assignment_text[this.assignment]);
+            console.log(this.textbox.text);
+            if (this.textbox.text == this.assignment_text[this.assignment]) {
+                console.log ("Well done!")
+            } else
+                console.log ("Poop job");
+        })
 
         // Add interactive textbox
         this.text_box_config = {
@@ -20,9 +41,8 @@ class Canvas extends Phaser.Scene {
             text_size: 32,
         };
 
-        this.textbox = this.add.interactiveTextBox(width / 2 , height * 0.8, this.text_box_config);
+        this.textbox = this.add.interactiveTextBox(width / 2 , height * 0.75, this.text_box_config);
         this.textbox.setInteractive();
-        //this.textbox.destroy();
     }
 
     update() {
