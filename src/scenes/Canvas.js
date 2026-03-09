9e19fde5acfee0f538131436fe77c1d70da8a54e
Max Kinet <mkinet@ucsc.edu>
2026-03-09 12:14:47 -0700

    add text color support

--- src/scenes/Canvas.js
+++ src/scenes/Canvas.js
@@ -14,10 +14,10 @@ class Canvas extends Phaser.Scene {
         this.text_box_config = {
             width: TEXTBOX_WIDTH,
             height: TEXTBOX_HEIGHT,
-            color: 0xb5bcbc,
             stroke_thickness: 3,
             stroke_color: COLORS.BLUE,
-            text_padding: TEXTBOX_PADDING
+            text_padding: TEXTBOX_PADDING,
+            text_size: 32,
         };
 
         this.textbox = this.add.interactiveTextBox(width / 2 , height * 0.8, this.text_box_config);
