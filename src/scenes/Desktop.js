fffdb838192f02ccd1d78c82842708448abca570
Max Kinet <mkinet@ucsc.edu>
2026-03-12 23:17:35 -0700

    add toolbar to desktop

--- src/scenes/Desktop.js
+++ src/scenes/Desktop.js
@@ -5,8 +5,10 @@ export class Desktop extends Phaser.Scene {
     }
 
     create() {
-        // add Desktop
+        // add Desktop and toolbar
         this.add.image(globals.width / 2, globals.height / 2, 'wxp_wallpaper').setDisplaySize(globals.width, globals.height);
+        this.toolbar = this.add.image(0, globals.height, 'wxp_toolbar').setDisplaySize(globals.width, globals.height * 0.04).setOrigin(0, 1);
+
 
         // Play background buzz
         this.sound.add('fluroscent_buzz', {loop: true, volume: 2.5}).play();
